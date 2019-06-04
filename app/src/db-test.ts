e2900f86d3cf59c718c0c02d84b641965b1a6c4b
Vincent den Boer <vincent@shishkabab.net>
2019-06-04 15:26:09 +0200

    Storex test now runs

--- app/src/db-test.ts
+++ app/src/db-test.ts
@@ -1,25 +1,25 @@
-// import { createConnection, getRepository, ManyToMany, JoinTable, ManyToOne } from 'typeorm/browser';
-// import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from "typeorm/browser";
 import StorageManager from '@worldbrain/storex'
 import { TypeORMStorageBackend } from '@worldbrain/storex-backend-typeorm'
 
 export async function runDatabaseTest() {
-    const backend = new TypeORMStorageBackend({ connectionOptions: { type: 'sqlite', database: ':memory:' } })
+    const backend = new TypeORMStorageBackend({ connectionOptions: { type: 'react-native', location: 'default', database: ':memory:' } })
     const storageManager = new StorageManager({ backend })
     storageManager.registry.registerCollections({
         post: {
             version: new Date(),
             fields: {
                 title: { type: 'string' },
-                text: { type: 'string' }
+                body: { type: 'string' }
             }
         },
     })
     await storageManager.finishInitialization()
+    await backend.connection.dropDatabase()
+    await storageManager.backend.migrate()
 
     const { object } = await storageManager.collection('post').createObject({ title: 'Post title', body: 'Post body' })
     console.log('Created object', object)
 
-    const loadedPost = await storageManager.collection('post').findObject({ id: object.id })
-    console.log('Found object', loadedPost)
+    const loadedPosts = await storageManager.collection('post').findObjects({})
+    console.log('Found objects', loadedPosts)
 }
