7ec07aea9ba5ff212d24e88d575ec7c6216a70b4
Vincent den Boer <vincent@shishkabab.net>
2019-06-05 18:05:18 +0200

    Add an example

--- app/src/features/example/ui/components/dumb.tsx
+++ app/src/features/example/ui/components/dumb.tsx
@@ -1,3 +1,11 @@
-export function TodoList() {
-    
+import React from 'react';
+import { Text } from "react-native";
+
+interface Props {
+
+}
+export function TodoList(props : Props) {
+    return (
+        <Text>Only stateless components are supposed to live here</Text>
+    )
 }
