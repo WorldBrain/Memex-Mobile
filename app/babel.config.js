a4050ca44805fed2796c4dcae9dcfe58efdf0833
Vincent den Boer <vincent@shishkabab.net>
2019-06-05 11:37:26 +0200

    Automatic version of babel config with external dirs

--- app/babel.config.js
+++ app/babel.config.js
@@ -1,3 +1,48 @@
+const fs = require('fs')
+const path = require('path')
+
+const EXTERNAL_DIR_PATH = path.join(__dirname, 'external')
+
+function getExternalPackageNames() {
+  const topLevelDirs = fs.readdirSync(EXTERNAL_DIR_PATH)
+  
+  const externalPackageNames = []
+  for (let topLevelDir of topLevelDirs) {
+    if (topLevelDir.charAt(0) === '@') {
+      const namespaceDirPath = path.join(EXTERNAL_DIR_PATH, topLevelDir)
+      const packageDirs = fs.readdirSync(namespaceDirPath)
+      for (let packageDir of packageDirs) {
+        externalPackageNames.push([topLevelDir, packageDir].join('/'))
+      }
+    } else {
+      externalPackageNames.push(topLevelDir)
+    }
+  }
+
+  return externalPackageNames
+}
+
+function isTypescriptPackage(packageName) {
+  const packageDirPath = path.join(EXTERNAL_DIR_PATH, packageName)
+  const tsDirPath = path.join(packageDirPath, 'ts')
+  return fs.existsSync(tsDirPath)
+}
+
+function getAliases() {
+  const aliases = {}
+  for (const packageName of getExternalPackageNames()) {
+    if (isTypescriptPackage(packageName)) {
+      Object.assign(aliases, {
+        [`^${packageName}$`]: `./external/${packageName}/ts`,
+        [`^${packageName}/lib(.+)$`]: `./external/${packageName}/ts\\1`,
+      })
+    }
+  }
+
+  // console.log(aliases)
+  return aliases
+}
+
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
@@ -5,12 +50,7 @@ module.exports = {
       require.resolve('babel-plugin-module-resolver'),
       {
         // root: ["./src/"],
-        alias: {
-          "^@worldbrain/storex$": "./external/@worldbrain/storex/ts",
-          "^@worldbrain/storex/lib(.+)$": "./external/@worldbrain/storex/ts\\1",
-          "^@worldbrain/storex-backend-typeorm$": "./external/@worldbrain/storex-backend-typeorm/ts",
-          "^@worldbrain/storex-backend-typeorm/lib(.+)$": "./external/@worldbrain/storex-backend-typeorm/ts\\1",
-        }
+        alias: getAliases()
       }
     ],
   ]
