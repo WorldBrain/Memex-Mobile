6f9336f8cc1a3647288bb7afa622d0a7948c8470
Vincent den Boer <vincent@shishkabab.net>
2019-06-05 11:14:16 +0200

    First version of working config

--- app/babel.config.js
+++ app/babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-};
+  plugins: [
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        // root: ["./src/"],
+        alias: {
+          "^@worldbrain/storex$": "./external/@worldbrain/storex/ts",
+          "^@worldbrain/storex/lib(.+)$": "./external/@worldbrain/storex/ts\\1",
+          "^@worldbrain/storex-backend-typeorm$": "./external/@worldbrain/storex-backend-typeorm/ts",
+          "^@worldbrain/storex-backend-typeorm/lib(.+)$": "./external/@worldbrain/storex-backend-typeorm/ts\\1",
+        }
+      }
+    ],
+  ]
+}
