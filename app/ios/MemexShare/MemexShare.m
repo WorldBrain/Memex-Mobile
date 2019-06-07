fd5d364e6cbb4ef31734aaa0aca7f6d2e8ac5e67
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2019-06-07 15:29:13 +0700

    Add iOS share extension
    - after a bit of pain, it now builds and runs on emulator fine

---
+++ app/ios/MemexShare/MemexShare.m
@@ -0,0 +1,39 @@
+//
+//  MemexShare.m
+//  MemexShare
+//
+//  Created by Jonathan Poltak Samosir on 7/6/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "ReactNativeShareExtension.h"
+#import <React/RCTBundleURLProvider.h>
+#import <React/RCTRootView.h>
+#import <React/RCTLog.h>
+
+@interface MemexShare : ReactNativeShareExtension
+@end
+
+@implementation MemexShare
+
+RCT_EXPORT_MODULE();
+
+- (UIView*) shareView {
+  NSURL *jsCodeLocation;
+  
+  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+  
+  RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
+                                                      moduleName:@"MemexShare"
+                                               initialProperties:nil
+                                                   launchOptions:nil];
+  rootView.backgroundColor = nil;
+  
+  // Uncomment for console output in Xcode console for release mode on device:
+  // RCTSetLogThreshold(RCTLogLevelInfo - 1);
+  
+  return rootView;
+}
+
+@end
