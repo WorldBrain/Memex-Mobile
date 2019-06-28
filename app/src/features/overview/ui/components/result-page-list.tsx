3c8de3ee63c26039155e7b3edb3f0da16318c54c
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2019-06-28 11:03:06 +0700

    Write view for main page result list

---
+++ app/src/features/overview/ui/components/result-page-list.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { FlatList, ListRenderItem } from 'react-native'
+
+import ResultPage, { Props as PageProps } from './result-page'
+import { NativeTouchEventHandler } from '../../types'
+
+export interface Props {
+    pages: PageProps[]
+    initPageDelete: (page: PageProps) => NativeTouchEventHandler
+    initPageTag: (page: PageProps) => NativeTouchEventHandler
+    initPageComment: (page: PageProps) => NativeTouchEventHandler
+    initPageStar: (page: PageProps) => NativeTouchEventHandler
+}
+
+class ResultPageList extends React.PureComponent<Props> {
+    private renderPage: ListRenderItem<PageProps> = ({ item, index }) => (
+        <ResultPage
+            onDeletePress={this.props.initPageDelete(item)}
+            onTagPress={this.props.initPageTag(item)}
+            onCommentPress={this.props.initPageComment(item)}
+            onStarPress={this.props.initPageStar(item)}
+            key={index}
+            {...item}
+        />
+    )
+
+    render() {
+        return (
+            <FlatList
+                renderItem={this.renderPage}
+                data={this.props.pages}
+            />
+        )
+    }
+}
+
+export default ResultPageList
