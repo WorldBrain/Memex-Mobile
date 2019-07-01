352112eb86fb0ff2f2ca8d1aee8088f7d4cb446e
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2019-07-01 10:53:55 +0700

    Write sectioned list view for notes results view

---
+++ app/src/features/overview/ui/components/result-note-list.styles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    sectionText: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginTop: 20,
+    },
+    pageList: {
+        width: '90%',
+    }
+})
