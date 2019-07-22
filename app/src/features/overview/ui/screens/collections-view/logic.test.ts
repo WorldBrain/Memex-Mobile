651cef6c93e6c41cf1112c13270fb4cd693a473a
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2019-07-22 16:35:01 +0800

    Add tests for more misc UI logic classes
    - most of the easier ones

--- app/src/features/overview/ui/screens/collections-view/logic.test.ts
+++ app/src/features/overview/ui/screens/collections-view/logic.test.ts
@@ -1,1 +1,64 @@
-it.todo('should include the unit tests for this UI logic')
+import Logic from './logic'
+import { UICollection } from 'src/features/overview/types'
+
+const testCollections: UICollection[] = [
+    { id: 0, name: 'testA' },
+    { id: 1, name: 'testB' },
+    { id: 2, name: 'testC' },
+]
+
+describe('collections view UI logic tests', () => {
+    function setup() {
+        const logic = new Logic()
+        const state = logic.getInitialState()
+
+        return { logic, state }
+    }
+
+    it('should be able to set collections', () => {
+        const { logic, state } = setup()
+
+        const newState = logic.withMutation(
+            state,
+            logic.setCollections({
+                event: { collections: new Set(testCollections) },
+                previousState: state,
+            }),
+        )
+
+        expect([...state.collections]).not.toEqual(
+            expect.arrayContaining(testCollections),
+        )
+        expect([...newState.collections]).toEqual(
+            expect.arrayContaining(testCollections),
+        )
+    })
+
+    it('should be able to select collections', () => {
+        const { logic, state } = setup()
+
+        const newStateA = logic.withMutation(
+            state,
+            logic.setCollections({
+                event: { collections: new Set(testCollections) },
+                previousState: state,
+            }),
+        )
+
+        const newStateB = logic.withMutation(
+            state,
+            logic.selectCollection({
+                event: { name: testCollections[0].name },
+                previousState: newStateA,
+            }),
+        )
+
+        expect([...newStateA.collections]).toEqual(
+            expect.arrayContaining(testCollections),
+        )
+        expect(newStateA.selectedCollection).not.toEqual(
+            testCollections[0].name,
+        )
+        expect(newStateB.selectedCollection).toEqual(testCollections[0].name)
+    })
+})
--- app/src/features/overview/ui/screens/overview/logic.test.ts
+++ app/src/features/overview/ui/screens/overview/logic.test.ts
@@ -1,1 +1,76 @@
-it.todo('should include the unit tests for this UI logic')
+import Logic from './logic'
+
+describe('meta picker UI logic tests', () => {
+    function setup() {
+        const logic = new Logic()
+        const state = logic.getInitialState()
+
+        return { logic, state }
+    }
+
+    it('should be able to show collections view', () => {
+        const { logic, state } = setup()
+
+        const newStateA = logic.withMutation(
+            state,
+            logic.setShowCollectionsView({
+                event: { show: true },
+                previousState: state,
+            }),
+        )
+        const newStateB = logic.withMutation(
+            state,
+            logic.setShowCollectionsView({
+                event: { show: false },
+                previousState: newStateA,
+            }),
+        )
+
+        expect(newStateA.showCollectionsView).toBe(true)
+        expect(newStateB.showCollectionsView).toBe(false)
+    })
+
+    it('should be able to show side menu', () => {
+        const { logic, state } = setup()
+
+        const newStateA = logic.withMutation(
+            state,
+            logic.setShowSideMenu({
+                event: { show: true },
+                previousState: state,
+            }),
+        )
+        const newStateB = logic.withMutation(
+            state,
+            logic.setShowSideMenu({
+                event: { show: false },
+                previousState: newStateA,
+            }),
+        )
+
+        expect(newStateA.showSideMenu).toBe(true)
+        expect(newStateB.showSideMenu).toBe(false)
+    })
+
+    it('should be able to set shown results type', () => {
+        const { logic, state } = setup()
+
+        const newStateA = logic.withMutation(
+            state,
+            logic.setResultType({
+                event: { resultType: 'notes' },
+                previousState: state,
+            }),
+        )
+        const newStateB = logic.withMutation(
+            state,
+            logic.setResultType({
+                event: { resultType: 'pages' },
+                previousState: newStateA,
+            }),
+        )
+
+        expect(newStateA.selectedResultType).toEqual('notes')
+        expect(newStateB.selectedResultType).toEqual('pages')
+    })
+})
