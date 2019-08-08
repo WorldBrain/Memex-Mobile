8035e0e1d9ce87a7aa5fc24cfe4be601082093b7
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2019-08-08 11:25:03 +0200

    Install + config QR code packages
    - most of this is from following the instructions at:
    https://github.com/moaazsidat/react-native-qrcode-scanner#getting-started

--- app/android/app/src/main/java/io/worldbrain/MainApplication.java
+++ app/android/app/src/main/java/io/worldbrain/MainApplication.java
@@ -3,6 +3,7 @@ package io.worldbrain;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import org.reactnative.camera.RNCameraPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
         new SQLitePluginPackage(),   // register SQLite Plugin here
         new MainReactPackage(),
+            new RNCameraPackage(),
             new RNGestureHandlerPackage(),
         new SharePackage()  // register `react-native-share-extension` plugin here
       );
