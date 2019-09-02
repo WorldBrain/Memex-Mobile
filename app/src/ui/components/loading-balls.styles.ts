cd0081d105297e020c6370074ba6bbe632be6231
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2019-09-02 09:57:11 +0700

    Write react native animation for loading balls
    - not entirely perfect yet, but I feel I got fairly far from 0 knowledge of how this works before

--- app/src/ui/components/loading-balls.styles.ts
+++ app/src/ui/components/loading-balls.styles.ts
@@ -2,23 +2,19 @@ import { StyleSheet } from 'react-native'
 
 export default StyleSheet.create({
     container: {
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'space-evenly',
-        alignItems: 'center',
+        position: 'relative',
         width: 80,
     },
     ballSelected: {
         backgroundColor: '#36362f',
     },
     ball: {
+        position: 'absolute',
         backgroundColor: '#c4c4c4',
         borderRadius: 25,
         width: 15,
         height: 15,
-    },
-    ballBig: {
-        width: 20,
-        height: 20,
+        top: '50%',
+        bottom: '50%',
     },
 })
--- app/src/ui/components/loading-balls.tsx
+++ app/src/ui/components/loading-balls.tsx
@@ -1,16 +1,82 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, Animated } from 'react-native'
 
 import styles from './loading-balls.styles'
 
 export interface Props {}
 
-const MainLayout: React.StatelessComponent<Props> = props => (
-    <View style={styles.container}>
-        <View style={styles.ball} />
-        <View style={[styles.ball, styles.ballBig]} />
-        <View style={styles.ball} />
-    </View>
-)
+interface State {
+    leftOffset: Animated.Value
+    sizeInc: Animated.Value
+    sizeDec: Animated.Value
+}
 
-export default MainLayout
+class LoadingBalls extends React.PureComponent<Props, State> {
+    static INIT_SIZE = 0
+    static FINAL_SIZE = 15
+    static INIT_SPACING = 0
+    static FINAL_SPACING = 30
+    static DURATION = 600
+
+    state = {
+        leftOffset: new Animated.Value(LoadingBalls.INIT_SPACING),
+        sizeInc: new Animated.Value(LoadingBalls.INIT_SIZE),
+        sizeDec: new Animated.Value(LoadingBalls.FINAL_SIZE),
+    }
+
+    componentDidMount() {
+        Animated.loop(
+            Animated.parallel([
+                Animated.timing(this.state.leftOffset, {
+                    toValue: LoadingBalls.FINAL_SPACING,
+                    duration: LoadingBalls.DURATION,
+                }),
+                Animated.timing(this.state.sizeInc, {
+                    toValue: LoadingBalls.FINAL_SIZE,
+                    duration: LoadingBalls.DURATION,
+                }),
+                Animated.timing(this.state.sizeDec, {
+                    toValue: LoadingBalls.INIT_SIZE,
+                    duration: LoadingBalls.DURATION,
+                }),
+            ]),
+        ).start()
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Animated.View
+                    style={{
+                        ...styles.ball,
+                        width: this.state.sizeInc,
+                        height: this.state.sizeInc,
+                        left: 0,
+                    }}
+                />
+                <Animated.View
+                    style={{
+                        ...styles.ball,
+                        left: Animated.add(0, this.state.leftOffset),
+                    }}
+                />
+                <Animated.View
+                    style={{
+                        ...styles.ball,
+                        left: Animated.add(30, this.state.leftOffset),
+                    }}
+                />
+                <Animated.View
+                    style={{
+                        ...styles.ball,
+                        width: this.state.sizeDec,
+                        height: this.state.sizeDec,
+                        left: 60,
+                    }}
+                />
+            </View>
+        )
+    }
+}
+
+export default LoadingBalls
