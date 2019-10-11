8e0ca8a0609742a44c02560c6f1f58d9eb65bc2d
Vincent den Boer <vincent@shishkabab.net>
2019-10-11 10:23:15 +0200

    Upgrade React Native

--- app/src/storage/server.ts
+++ app/src/storage/server.ts
@@ -1,11 +1,11 @@
-import { firebase } from '@react-native-firebase/firestore'
+import firestore from '@react-native-firebase/firestore'
 import StorageManager from '@worldbrain/storex'
 import { FirestoreStorageBackend } from '@worldbrain/storex-backend-firestore'
 
 export function createServerStorageManager() {
     const serverStorageBackend = new FirestoreStorageBackend({
-        firebase: firebase as any,
-        firestore: firebase.firestore() as any,
+        firebase: { firestore } as any,
+        firestore: firestore() as any,
     })
     return new StorageManager({ backend: serverStorageBackend })
 }
