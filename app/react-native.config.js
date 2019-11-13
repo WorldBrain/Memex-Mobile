0edcfeefd571c670a99afea3715f98c44527287b
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2019-11-13 11:29:51 +0700

    Fix broken iOS build
    - added Firebase, ReactNativePermissions dep libs to CocoaPods dep mgmt
    - set main app build to inherit linker flags
    - removed a bunch of explicit deps in the xcode build targets that are now managed by CocoaPods

---
+++ app/react-native.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    dependencies: {
+        'react-native-camera': {
+            platforms: {
+                android: null,
+                ios: null,
+            },
+        },
+        'react-native-share-extension': {
+            platforms: {
+                android: null,
+                ios: null,
+            },
+        },
+        'react-native-webrtc': {
+            platforms: {
+                android: null,
+                ios: null,
+            },
+        },
+        '@react-native-firebase/app': {
+            platforms: {
+                android: null,
+                ios: null,
+            },
+        },
+    },
+}
