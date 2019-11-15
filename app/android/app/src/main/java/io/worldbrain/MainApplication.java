b429edc1d23cc595a43eb70abaeb3fb010f5ccac
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2019-11-15 13:28:35 +0700

    Fix android build
    - essentially re-links most of the RN packages, so you'll get warnings when you run `react-native run-android` that all the packages are linked
    - want to look into it further to see how to properly unlink them without breaking the build

--- app/android/app/src/main/java/io/worldbrain/MainApplication.java
+++ app/android/app/src/main/java/io/worldbrain/MainApplication.java
@@ -3,11 +3,21 @@ package io.worldbrain;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oney.WebRTCModule.WebRTCModulePackage;
+import io.invertase.firebase.firestore.ReactNativeFirebaseFirestorePackage;
+import io.invertase.firebase.database.ReactNativeFirebaseDatabasePackage;
+import io.invertase.firebase.auth.ReactNativeFirebaseAuthPackage;
+import io.invertase.firebase.app.ReactNativeFirebaseAppPackage;
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
+import org.reactnative.camera.RNCameraPackage;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
+import org.pgsqlite.SQLitePluginPackage;
+import com.alinz.parkerdan.shareextension.SharePackage;
 
 import java.util.Arrays;
 import java.util.List;
