3d0505829518e417383d9406aa658a680f0d1cd5
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2019-12-05 10:04:15 +0700

    Fix incorrectly typed component

--- app/src/features/overview/ui/screens/pairing-screen/index.tsx
+++ app/src/features/overview/ui/screens/pairing-screen/index.tsx
@@ -3,9 +3,11 @@ import { Alert } from 'react-native'
 
 import SyncSuccess from 'src/features/sync/ui/components/sync-success-stage'
 import { storageKeys } from '../../../../../../app.json'
-import { NavigationScreen, NavigationProps } from 'src/ui/types'
+import { NavigationScreen, NavigationProps, UIServices } from 'src/ui/types'
 
-interface Props extends NavigationProps {}
+interface Props extends NavigationProps {
+    services: UIServices<'localStorage'>
+}
 
 export default class RePairScreen extends NavigationScreen<Props, {}, Event> {
     private handleRePairConfirmation = async () => {
