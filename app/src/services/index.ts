9875f41501f6e600ddd93441c1a900d2d4be2f86
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2019-12-11 11:21:56 +0700

    Fix up some type issues in service entrypoint
    - these may have been here for a while, but shouldn't have been affecting any of the bugs we've run into

--- app/src/services/index.ts
+++ app/src/services/index.ts
@@ -1,10 +1,12 @@
 import { SharedSyncLog } from '@worldbrain/storex-sync/lib/shared-sync-log'
+import { SignalTransportFactory } from '@worldbrain/memex-common/lib/sync'
+import { AuthService } from '@worldbrain/memex-common/lib/authentication/types'
+import { MemexSyncDevicePlatform } from '@worldbrain/memex-common/lib/sync/types'
 
 import { Services } from './types'
 import { ShareExtService } from './share-ext'
 import { LocalStorageService } from './local-storage'
 import SyncService from './sync'
-import { SignalTransportFactory } from './sync/initial-sync'
 import { Storage } from 'src/storage/types'
 import { AuthService } from '@worldbrain/memex-common/lib/authentication/types'
 import { MemexSyncDevicePlatform } from '@worldbrain/memex-common/lib/sync/types'
