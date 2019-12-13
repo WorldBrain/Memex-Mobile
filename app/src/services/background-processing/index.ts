fb932f1468d5a07c01eacf6c489677d07b5ef13d
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2019-12-13 12:52:22 +0700

    Allow background processing to be paused and resumed

--- app/src/services/background-processing/index.ts
+++ app/src/services/background-processing/index.ts
@@ -15,6 +15,7 @@ export interface Props {
 export class BackgroundProcessService {
     static DEF_PROCESSING_INTERVAL_MIN = 15
 
+    private _process?: () => Promise<void>
     private backgroundAPI: typeof BackgroundFetch
     private config: BackgroundFetchConfig
 
@@ -32,30 +33,53 @@ export class BackgroundProcessService {
         }
     }
 
-    async scheduleProcess(process: BackgroundProcess) {
+    scheduleProcess(process: BackgroundProcess) {
+        this._process = async () => {
+            const { newData } = await process()
+
+            this.backgroundAPI.finish(
+                newData
+                    ? this.backgroundAPI.FETCH_RESULT_NEW_DATA
+                    : this.backgroundAPI.FETCH_RESULT_NO_DATA,
+            )
+        }
+
+        this.scheduleSetProcess()
+    }
+
+    forceRun() {
+        if (!this._process) {
+            throw new Error('No process scheduled to run.')
+        }
+
+        return this._process()
+    }
+
+    pause(): () => void {
+        if (!this._process) {
+            throw new Error('No process scheduled to pause.')
+        }
+
+        this.backgroundAPI.stop()
+
+        return this.scheduleSetProcess
+    }
+
+    private scheduleSetProcess = () =>
         this.backgroundAPI.configure(
             this.config,
-            async () => {
-                const { newData } = await process()
-
-                this.backgroundAPI.finish(
-                    newData
-                        ? BackgroundFetch.FETCH_RESULT_NEW_DATA
-                        : BackgroundFetch.FETCH_RESULT_NO_DATA,
-                )
-            },
+            this._process!,
             this.handleProcessErrors,
         )
-    }
 
     private handleProcessErrors = (status: BackgroundFetchStatus) => {
         console.error('Background processing encountered an error.')
 
-        if (status === BackgroundFetch.STATUS_RESTRICTED) {
+        if (status === this.backgroundAPI.STATUS_RESTRICTED) {
             console.error(
                 'Background processing is currently unavailable due to device restriction settings (i.e., parental controls).',
             )
-        } else if (status === BackgroundFetch.STATUS_DENIED) {
+        } else if (status === this.backgroundAPI.STATUS_DENIED) {
             console.error(
                 'Background processing is currently disabled for this app or the whole system.',
             )
