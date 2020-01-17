47837cc7ca6b2f61018775a8377bbe90a93ac347
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2020-01-17 13:33:44 +0700

    Add ssh-add usage to write keys script

--- write-keys.sh
+++ write-keys.sh
@@ -2,7 +2,14 @@
 
 PRIVATE_KEY=~/.ssh/id_rsa
 PUBLIC_KEY=~/.ssh/id_rsa.pub
+SSH_CONF=~/.ssh/config
 
 echo $IOS_REPO_PRIVATE_KEY | base64 -D > $PRIVATE_KEY
 echo $IOS_REPO_PUBLIC_KEY | base64 -D > $PUBLIC_KEY
+
+echo "Host *" >> $SSH_CONF
+echo "    UseKeychain yes" >> $SSH_CONF
+echo "    AddKeysToAgent yes"  >> $SSH_CONF
+
 chmod 400 $PRIVATE_KEY
+ssh-add $PRIVATE_KEY
