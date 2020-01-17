0ad0cfbfc480687d3a67d3dcc86f3986e750e0eb
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2020-01-17 13:08:22 +0700

    Update write script to decode base64 keys

--- write-keys.sh
+++ write-keys.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 PRIVATE_KEY=~/.ssh/id_rsa
+PUBLIC_KEY=~/.ssh/id_rsa.pub
 
-echo "$IOS_REPO_PRIVATE_KEY" > $PRIVATE_KEY
-sed -i '.bak' 's/\\ /\ /g' $PRIVATE_KEY
+echo $IOS_REPO_PRIVATE_KEY | base64 -D > $PRIVATE_KEY
+echo $IOS_REPO_PUBLIC_KEY | base64 -D > $PUBLIC_KEY
 chmod 400 $PRIVATE_KEY
