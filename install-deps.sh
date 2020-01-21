a27ff70d7832b9cb5a5fa616311db987b22fa9fb
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2020-01-21 11:29:38 +0700

    Update travis install deps for android

--- install-deps.sh
+++ install-deps.sh
@@ -3,8 +3,27 @@
 git submodule update --init --recursive
 cd ./app/
 yarn install --frozen-lockfile
-cd ./ios/
 
-if [ $# -eq 1 ]; then
+usage()
+{
+    echo "usage: install-deps.sh [pods|gems]"
+    exit 1
+}
+
+if [ $# -ne 1 ]
+then
+    usage
+fi
+
+if [ $1 == "pods" ]
+then
+    cd ./ios/
     pod install
+    cd ..
+elif [ $1 == "gems" ]
+then
+    bundle install
+else
+    echo "Unknown args"
+    usage
 fi
