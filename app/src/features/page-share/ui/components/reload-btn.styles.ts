a0a9326f4e94743c1f18c5005b27717adbf716c9
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2020-03-06 15:57:55 +0700

    Fix invalid style rule
    - `background` should have been `backgroundColor`

--- app/src/features/page-share/ui/components/reload-btn.styles.ts
+++ app/src/features/page-share/ui/components/reload-btn.styles.ts
@@ -2,7 +2,7 @@ import EStyleSheet from 'react-native-extended-stylesheet'
 
 export default EStyleSheet.create({
     container: {
-        background: 'white',
+        backgroundColor: 'white',
     },
     img: {
         width: 15,
