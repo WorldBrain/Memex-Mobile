333484fb1cb3d2314ccede5230a0156999dd600e
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2020-03-06 12:35:28 +0700

    Attempt to fix file icon behind reload btn
    - it's only partially fixed by putting a white BG: when you click it, it becomes transparent and you can see the file icon underneath still :/
    - another solution could be to disable transparency on press
    - I have no idea why that's there...

--- app/src/features/page-share/ui/components/reload-btn.styles.ts
+++ app/src/features/page-share/ui/components/reload-btn.styles.ts
@@ -1,8 +1,10 @@
 import EStyleSheet from 'react-native-extended-stylesheet'
 
 export default EStyleSheet.create({
-    container: {},
-    Img: {
+    container: {
+        background: 'white',
+    },
+    img: {
         width: 15,
         marginLeft: 15,
         marginRight: 15,
