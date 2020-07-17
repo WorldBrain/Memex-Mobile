5e5ba177a1b8b36f2152d5eef09cb75de60e2111
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2020-07-17 11:17:20 +0700

    Add quick-n-dirty migration for `clientSyncLogEntry` coll

--- app/src/utils/quick-and-dirty-migrations.ts
+++ app/src/utils/quick-and-dirty-migrations.ts
@@ -1,6 +1,7 @@
 import StorageManager from '@worldbrain/storex'
 
 import { UIDependencies } from 'src/ui/types'
+import { ClientSyncLogEntry } from '@worldbrain/storex-sync/lib/client-sync-log/types'
 
 export interface MigrationProps {
     db: StorageManager
@@ -16,6 +17,34 @@ export const migrations: Migrations = {
         await db.collection('visits').deleteObjects({ url: '' })
         await db.collection('annotations').deleteObjects({ pageUrl: '' })
         await db.collection('pageListEntries').deleteObjects({ pageUrl: '' })
+    },
+    'fill-out-empty-sync-log-fields': async ({ db }) => {
+        const limit = 100
+        let iterations = 0
+        let exhausted = false
+
+        // Go through the collection in chunks of `limit`, updating each record in each chunk
+        do {
+            const entries: ClientSyncLogEntry[] = await db
+                .collection('clientSyncLogEntry')
+                .findObjects({}, { limit, skip: iterations * limit })
+
+            for (const entry of entries) {
+                await db.collection('clientSyncLogEntry').updateOneObject(
+                    {
+                        deviceId: entry.deviceId,
+                        createdOn: entry.createdOn,
+                    },
+                    {
+                        sharedOn: entry.sharedOn ?? 0,
+                        needsIntegration: entry.needsIntegration ? 1 : 0,
+                    },
+                )
+            }
+
+            exhausted = entries.length < limit
+            iterations++
+        } while (!exhausted)
     },
 }
 
