9e3ec8d39b04e7f606c628d0028b307a33ae40fd
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2020-09-29 11:09:32 +0700

    Get rid of some really old dead screens

--- app/src/features/overview/ui/screens/pages-view/selectors.ts
+++
@@ -1,5 +0,0 @@
-import { State } from './logic'
-import { UIPage } from 'src/features/overview/types'
-
-export const pages = (state: State) => state.pages
-export const results = (state: State): UIPage[] => [...pages(state).values()]
