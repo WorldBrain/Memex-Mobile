0fd1eb4b6ad0b2eb62ef975c98894a5a3029ee41
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2021-08-16 14:59:12 +0700

    Fix some badly formatted test data failing sync tests

--- app/src/features/page-editor/ui/screens/page-editor/logic.test.data.ts
+++ app/src/features/page-editor/ui/screens/page-editor/logic.test.data.ts
@@ -3,11 +3,12 @@ export const PAGE_1 = {
     domain: 'test.com',
     hostname: 'test.com',
     fullUrl: 'http://test.com',
+    canonicalUrl: 'http://test.com',
     fullTitle: 'Test Page',
 }
 
 export const NOTE_1 = {
-    url: PAGE_1.url + '#123',
+    url: PAGE_1.url + '/#123',
     pageUrl: PAGE_1.url,
     pageTitle: PAGE_1.fullTitle,
     comment: 'This is a test note',
