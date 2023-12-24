141e7f5db3d484e8ff56fd503f1a7b561a79b936
Oliver Sauter <oli@worldbrain.io>
2023-12-24 10:48:03 -0800

    Fix annotation editor crashing on android

--- app/src/features/page-share/ui/components/note-input-segment.tsx
+++ app/src/features/page-share/ui/components/note-input-segment.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components/native'
 import QuillEditor, { QuillToolbar } from 'react-native-cn-quill'
 import { StyleSheet } from 'react-native'
+import LoadingBalls from 'src/ui/components/loading-balls'
+const LoadingBox = styled.View`
+    margin-top: 40px;
+`
 
 export interface Props {
     containerClassName?: string
@@ -13,70 +17,53 @@ export interface Props {
 }
 
 const NoteInput: React.StatelessComponent<Props> = (props) => {
-    const _editor = React.createRef()
+    const editor = React.createRef()
+    const [renderEditor, setRenderEditor] = useState(false)
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setRenderEditor(true)
+        }, 500) // 1000ms delay
+
+        return () => clearTimeout(timer) // cleanup on unmount
+    }, [])
 
     return (
         <Container>
             <TextBox>
-                <QuillToolbar
-                    style={styles.toolbar}
-                    editor={_editor}
-                    theme="light"
-                    options={[
-                        'bold',
-                        'italic',
-                        'underline',
-                        'strike',
-                        { header: 1 },
-                        { header: 2 },
-                        { list: 'ordered' },
-                        { list: 'bullet' },
-                    ]}
-                />
-                <QuillEditor
-                    style={styles.editor}
-                    ref={_editor}
-                    initialHtml={props.initNote}
-                    onHtmlChange={({ html }) => {
-                        props.onChange(html)
-                    }}
-                />
-
-                {/* <TextInputContainer
-                    value={props.value}
-                    onChangeText={props.onChange}
-                    textAlignVertical="top"
-                    placeholder="Add Note"
-                    editable={!props.disabled}
-                    placeholderTextColor={theme.colors.greyScale6}
-                    multiline
-                    autoFocus
-                /> */}
+                <Box>
+                    <QuillToolbar
+                        editor={editor}
+                        theme="dark"
+                        options={[
+                            'bold',
+                            'italic',
+                            'underline',
+                            'strike',
+                            { header: 1 },
+                            { header: 2 },
+                            { list: 'ordered' },
+                            { list: 'bullet' },
+                        ]}
+                    />
+                </Box>
+                {renderEditor ? (
+                    <QuillEditor
+                        ref={editor}
+                        initialHtml={props.initNote}
+                        onHtmlChange={({ html }) => {
+                            props.onChange(html)
+                        }}
+                    />
+                ) : (
+                    <LoadingBox>
+                        <LoadingBalls />
+                    </LoadingBox>
+                )}
             </TextBox>
         </Container>
     )
 }
-
-const styles = StyleSheet.create({
-    title: {
-        fontWeight: 'bold',
-        alignSelf: 'center',
-    },
-    root: {
-        flex: 1,
-    },
-    editor: {
-        flex: 1,
-        padding: 0,
-        backgroundColor: '#191a20',
-    },
-    toolbar: {
-        flex: 1,
-        borderRadius: 6,
-        backgroundColor: '#191a20',
-        color: 'red',
-    },
-})
 
 const Container = styled.SafeAreaView`
     flex: 1;
@@ -84,35 +71,24 @@ const Container = styled.SafeAreaView`
     flex-direction: column;
     align-items: center;
     justify-content: flex-start;
+    background: ${(props) => props.theme.colors.black};
 `
 
 const TextBox = styled.View`
-    background: ${(props) => props.theme.colors.greyScale1};
     border-top-left-radius: 8px;
     border-top-right-radius: 8px;
     border-bottom-left-radius: 0px;
     border-bottom-right-radius: 0px;
     width: 100%;
     flex: 1;
-    border: none;
     width: 800px;
     max-width: 100%;
-    color: ${(props) => props.theme.colors.white};
-
-    ::placeholder {
-        color: ${(props) => props.theme.colors.greyScale6};
-    }
+    background: ${(props) => props.theme.colors.black};
 `
 
-const TextInputContainer = styled.TextInput`
-    padding: 20px 20px 100px 20px;
-    flex: 1;
-    font-size: 15px;
-    color: ${(props) => props.theme.colors.greyScale6};
-
-    ::placeholder {
-        color: ${(props) => props.theme.colors.greyScale6};
-    }
+const Box = styled.View`
+    margin-top: -2px;
+    margin-left: -2px;
 `
 
 export default NoteInput
