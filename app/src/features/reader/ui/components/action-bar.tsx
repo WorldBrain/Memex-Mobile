832f11f8cb8750ece8fdc96df4333f3955138bf3
Oliver Sauter <oli@worldbrain.io>
2023-12-27 21:46:21 -0800

    fix centering of the buttons in the bottom bar

--- app/src/features/reader/ui/components/action-bar.tsx
+++ app/src/features/reader/ui/components/action-bar.tsx
@@ -237,6 +237,9 @@ const FooterActionBtn = styled.TouchableOpacity`
     display: flex;
     padding: 10px;
     position: relative;
+    width: 75px;
+    justify-content: center;
+    align-items: center;
 `
 const FooterActionBar = styled.View<{
     os: 'ios' | 'android'
@@ -245,7 +248,7 @@ const FooterActionBar = styled.View<{
 }>`
     display: flex;
     flex-direction: row;
-    justify-content: space-between;
+    justify-content: center;
     background: ${(props) => props.theme.colors.greyScale1};
     border-style: solid;
     border-top-width: 1px;
