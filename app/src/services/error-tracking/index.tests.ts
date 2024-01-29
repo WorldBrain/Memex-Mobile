8c5166daa2fab6815ef00e136defc4b1c9e311a0
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2024-01-29 11:21:47 +0700

    Add setUser to Sentry mock

--- app/src/services/error-tracking/index.tests.ts
+++ app/src/services/error-tracking/index.tests.ts
@@ -1,9 +1,19 @@
+import type { AuthenticatedUser } from '@worldbrain/memex-common/lib/authentication/types'
+
 export class MockSentry {
+    user: { id: string; email?: string } | null = null
     initArgs: any
     captured: any[] = []
 
     init(args: any) {
         this.initArgs = args
+    }
+
+    setUser(user: AuthenticatedUser | null) {
+        console.log('DEV: Setting sentry user:', user)
+        this.user = user
+            ? { id: user.id, email: user.email ?? undefined }
+            : null
     }
 
     captureException(err: any) {
